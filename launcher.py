84bafdab4eb378a054bc5e5a97645e37a20c5ace
Veselin Penev <penev.veselin@gmail.com>
2025-08-23 09:56:02 +0200

    added Windows launcher script

---
+++ launcher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python -u
+# -*- coding: utf-8 -*-
+
+"""
+This is a wrapper for Windows Desktops
+Runs git commands to update source code of the BitDust p2p-app before start
+"""
+
+import os
+import sys 
+import subprocess
+
+
+def run_cmd(cmd):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    return p.wait()
+
+
+def launch():
+    run_cmd(['..\\git\\bin\\git.exe', '-c', 'http.sslBackend=schannel', 'fetch', '--all', ])
+    run_cmd(['..\\git\\bin\\git.exe', '-c', 'http.sslBackend=schannel', 'reset', '--hard', 'origin/master', ])
+
+    sys.path.append('src')
+
+    from src.main import main 
+    return main()
+
+
+if __name__ == '__main__':
+    launch()
